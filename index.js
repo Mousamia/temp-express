2879701d3dfac58dfacbd32b1793713f8231ba99
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-05-11 03:39:34 +0600

    new data adding failed

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const  kijane = require('./data.json');
+const users = require('./user.json');
 const app = express()
+const cors = require('cors')
 const port = 5000
 
 
-// app.get('/', (req, res) =>{
-//     res.send('segun kajth')
-// })
+app.get('/', (req, res) =>{
+    res.send('hlw')
+})
 
+app.use(cors());
+// for reading body
+app.use(express.json())
 
 app.get('/kijane', (req, res) =>{
     res.send(kijane);
@@ -19,6 +24,17 @@ app.get('/kijane/:id', (req, res) =>{
     console.log('I need  data  for', id);
 })
 
+
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.post('/users', (req, res) =>{
+    console.log("daata comimg");
+    console.log(req.body)
+})
+
 app.listen(port, ()=>{
-    console.log(`Example app listensing on port ${port}`)
+    console.log(`Example app running on port ${port}`)
 })
