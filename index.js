66a57d326a26f0308c561a07d5f6ecd1f3b5c12f
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-05-11 14:34:38 +0600

    sending data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const  kijane = require('./data.json');
 const users = require('./user.json');
 const app = express()
 const cors = require('cors')
-const port = 5000
+const port = process.env.port || 5000
 
 
 app.get('/', (req, res) =>{
@@ -14,16 +14,6 @@ app.use(cors());
 // for reading body
 app.use(express.json())
 
-app.get('/kijane', (req, res) =>{
-    res.send(kijane);
-})
-
-app.get('/kijane/:id', (req, res) =>{
-    const id = parseInt(req.params.id);
-    // console.log(req.params);
-    console.log('I need  data  for', id);
-})
-
 
 
 app.get('/users', (req, res) => {
@@ -32,9 +22,28 @@ app.get('/users', (req, res) => {
 
 app.post('/users', (req, res) =>{
     console.log("daata comimg");
-    console.log(req.body)
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    console.log(newUser);
+    users.push(newUser);
+    res.send(newUser);
+    
+
 })
 
 app.listen(port, ()=>{
     console.log(`Example app running on port ${port}`)
+  
 })
+
+
+// app.get('/kijane', (req, res) =>{
+//     res.send(kijane);
+// })
+
+// app.get('/kijane/:id', (req, res) =>{
+//     const id = parseInt(req.params.id);
+//     // console.log(req.params);
+//     console.log('I need  data  for', id);
+// })
